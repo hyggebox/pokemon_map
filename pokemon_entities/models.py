35749d569d9b37086d57b0e10648105b822c511c
Anastasia <asya_apto@mail.ru>
2022-01-10 15:40:03 +0300

    Add blank=True to Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images/', null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
