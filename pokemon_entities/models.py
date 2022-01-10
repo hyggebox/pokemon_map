931cf711aa629dc2eb2f704a89b1f079393eef64
Anastasia <asya_apto@mail.ru>
2022-01-11 00:24:29 +0300

    Remove null=True from CharFields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,8 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True, null=True)
-    title_jp = models.CharField(max_length=200, blank=True, null=True)
+    title_en = models.CharField(max_length=200, blank=True)
+    title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     description = models.TextField(blank=True)
     previous_evolution = models.ForeignKey("self", on_delete=models.SET_NULL,
