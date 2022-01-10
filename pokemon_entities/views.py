9a2b4a5725d0368d0d88bb7acdee5dc717707f6a
Anastasia <asya_apto@mail.ru>
2022-01-10 17:31:31 +0300

    Add English and Japanese pokemon names

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -73,6 +73,8 @@ def show_pokemon(request, pokemon_id):
         )
     pokemon = {
         'title_ru': requested_pokemon.title,
+        'title_en': requested_pokemon.title_en,
+        'title_jp': requested_pokemon.title_jp,
         'img_url': request.build_absolute_uri(requested_pokemon.image.url),
         'description': requested_pokemon.description
     }
